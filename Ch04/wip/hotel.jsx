8ff0e064f4fbeb3e104c82486018f66b135b8dc1
Dipak Ghosh <70680058+DGreat49251@users.noreply.github.com>
2026-07-20 04:22:54 +0000

    chapter 4 API Usage and Integration

--- Ch04/wip/hotel.jsx
+++ Ch04/wip/hotel.jsx
@@ -1,22 +1,35 @@
-import React, { StrictMode, Fragment, useState } from "react";
+import React, { StrictMode, Fragment, useState, useEffect } from "react";
 import { createRoot } from "react-dom/client";
 import date from "date-and-time";
 
-const messageTypes = {
-	management: "Management",
-	dining: "Dining Services",
-	ops: "Operations",
-	plumbing: "Plumbing",
-	pool: "Pool",
+const CONFIG = {
+	messageTypes: {
+		management: "Management",
+		dining: "Dining Services",
+		ops: "Operations",
+		plumbing: "Plumbing",
+		pool: "Pool",
+	},
+
+	apiUrl: "/api",
+	dateTimeFormat: "YYYY-MM-DD HH:mm:ss",
+	dateTimeFieldFormat: "YYYY-MM-DDTHH:mm",
 };
 
-const apiUrl = "/api";
+function Loading({ what = "messages" }) {
+	return (
+		<p>
+			Loading {what}{" "}
+			<span className="loader" style={{ marginLeft: "0.5rem" }} />
+		</p>
+	);
+}
 
 function PostForm(props) {
-	const typeOptions = Object.keys(messageTypes).map(function (key) {
+	const typeOptions = Object.keys(CONFIG.messageTypes).map(function (key) {
 		return (
 			<option key={key} value={key}>
-				{messageTypes[key]}
+				{CONFIG.messageTypes[key]}
 			</option>
 		);
 	});
@@ -24,18 +37,93 @@ function PostForm(props) {
 	// so we don't have to type this over and over
 	const defaultType = typeOptions[0].key;
 
+	const [messageText, setMessageText] = useState("");
+	const [messageType, setMessageType] = useState(defaultType);
+	const [messageDateTime, setMessageDateTime] = useState(new Date());
+
+	function onTextChange(evt) {
+		setMessageText(evt.target.value);
+	}
+
+	function onTypeChange(evt) {
+		setMessageType(evt.target.value);
+	}
+
+	function onDateTimeChange(evt) {
+		const dateTime = date.parse(evt.target.value, dateTimeFieldFormat);
+		setMessageDateTime(dateTime);
+	}
+
+	function postStatusUpdate(evt) {
+		evt.preventDefault();
+		const messageDateTimeToPost = date.format(messageDateTime, CONFIG.dateTimeFormat);
+
+		const newStatus = {
+			msg: messageText,
+			type: messageType,
+			time: messageDateTimeToPost,
+		};
+
+		fetch(`${CONFIG.apiUrl}/messages`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(newStatus),
+		})
+			.then(function (response) {
+				console.log(response);
+
+				if (!response.ok) {
+					throw new Error(`Response Status: ${response.status}`);
+				}
+				return response.json();
+			})
+			.then(function (jsonData) {
+				newStatus.id = jsonData.message.id;
+				props.addStatusMessage(newStatus);
+				//reset the form values
+				setMessageText("");
+				setMessageType(defaultType);
+				setMessageDateTime(new Date());
+
+				console.log(jsonData);
+			})
+			.catch(function (error) {
+				console.error("Error posting status update", error);
+			});
+	}
+
 	return (
-		<form>
+		<form onSubmit={postStatusUpdate}>
 			<h3>Post an Update</h3>
 
 			<div className="field-group">
 				<label htmlFor="txt-message">Message</label>
-				<textarea id="txt-message" rows="2" required />
+				<textarea
+					id="txt-message"
+					rows="2"
+					required
+					onChange={onTextChange}
+					value={messageText}
+				/>
 			</div>
 
 			<div className="field-group">
 				<label htmlFor="txt-type">Type</label>
-				<select id="txt-type">{typeOptions}</select>
+				<select id="txt-type" onChange={onTypeChange}>
+					{typeOptions}
+				</select>
+			</div>
+
+			<div className="field-group">
+				<label htmlFor="txt-datetime">When</label>
+				<input
+					type="datetime-local"
+					id="txt-datetime"
+					onChange={onDateTimeChange}
+					value={date.format(messageDateTime, CONFIG.dateTimeFieldFormat)}
+				/>
 			</div>
 
 			<div className="field-group action">
@@ -80,34 +168,72 @@ function StatusMessageList(props) {
 		},
 	];
 
-	const [statuses, setStatuses] = useState(stubStatuses);
-
 	let displayedStatuses;
 
-	if (statuses.length > 0) {
-		displayedStatuses = statuses.map((status) => {
+	if (props.loading) {
+		displayedStatuses = <Loading />;
+	} else if (props.statuses.length > 0) {
+		let sortedStatuses = props.statuses.slice(0);
+		sortedStatuses.sort((a, b) => {
+			return new Date(b.time) - new Date(a.time);
+		});
+		displayedStatuses = props.statuses.map((status) => {
 			return (
 				<li key={status.id} className={status.type}>
 					<StatusMessage
 						msg={status.msg}
-						type={messageTypes[status.type]}
+						type={CONFIG.messageTypes[status.type]}
 						time={status.time}
 					/>
 				</li>
 			);
 		});
+	} else {
+		displayedStatuses = <Loading />;
 	}
 
 	return <ul id="status-list">{displayedStatuses}</ul>;
 }
 
 function StatusMessageManager() {
+	const [statuses, setStatuses] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		setLoading(true);
+		const abortController = new AbortController();
+
+		fetch(`${CONFIG.apiUrl}/messages?delay=5000`, {
+			signal: abortController.signal,
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				setStatuses(data);
+				setLoading(false);
+			})
+			.catch((error) => {
+				if (error.name !== "AbortError") {
+					console.error("Error fetching status messages: ", error);
+					setLoading(false);
+				}
+			});
+
+		return () => {
+			abortController.abort();
+		};
+	}, []);
+
+	function addStatusMessage(status) {
+		const updatedStatuses = statuses.slice(0); //Get an existing copy
+		updatedStatuses.push(status); //Insert the new one added
+		setStatuses(updatedStatuses); //Update
+	}
 	return (
 		<Fragment>
 			<div id="post-status">
-				<PostForm />
+				<PostForm addStatusMessage={addStatusMessage} />
 			</div>
-			<StatusMessageList />
+			<StatusMessageList statuses={statuses} loading={loading} />
 		</Fragment>
 	);
 }
